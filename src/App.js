958042b419a0a0432a7c41ab893ccd90493d2574
Arshey Belkhode <11.21.22.arsh@gmail.com>
2022-02-15 19:08:39 +0530

    Changed Switch to Route

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <>
-        <Switch>
-          <Route exact path="/">
-            <Welcome />
-          </Route>
-          <Route exact path="/instruction">
-            <Instruction />
-          </Route>
-          <Route exact path="/quiz">
-            <Quiz />
-          </Route>
-        </Switch>
-      </>
+      <Route>
+        <Route exact path="/">
+          <Welcome />
+        </Route>
+        <Route exact path="/instruction">
+          <Instruction />
+        </Route>
+        <Route exact path="/quiz">
+          <Quiz />
+        </Route>
+      </Route>
     </Router>
   );
 }
